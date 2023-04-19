bd75fc54be3303bd04d4c0af0dc7b4b1803599ed
Manjunath <manjunath@vhypotenuse.com>
2023-04-19 09:59:14 +0530

    updated

--- Main.php
+++ Main.php
@@ -4,7 +4,7 @@ use PersonAddress\Address;
 require_once "person_details.php";
 require "separatefile.php";
 
-$address = new Address('abc', 20, '12th main', 'btm', 'Bengaluru');
+$address = new Address('a', 20, '12th main', 'btm', 'Bengaluru');
 echo "person name : ".$address->getName() ."\n"; 
 echo "person age : ".$address->getAge() ."\n"; 
 echo "person address : ".$address->getAddress() ."\n"; 
