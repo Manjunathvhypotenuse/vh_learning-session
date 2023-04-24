10e5cf91aa86e19c6f72527aa7704aed87640918
Manjunath <manjunath@vhypotenuse.com>
2023-04-24 15:33:03 +0530

    task_8

--- database/migrations/2023_04_24_063937_create_categories_table.php
+++ database/migrations/2023_04_24_063937_create_categories_table.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table-> string("name", 200);
+            $table-> boolean("is_active");
+            $table-> integer("created_by")->nullable();
+            $table-> integer("updated_by")->nullable();
+            $table-> integer("deleted_by")->nullable();
             $table->timestamps();
+            $table->softDeletes(); 
         });
     }
 
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
