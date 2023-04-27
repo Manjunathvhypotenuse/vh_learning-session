f318e849cd4d173074d886bfef2fc59b6bf67619
Manjunath <manjunath@vhypotenuse.com>
2023-04-27 19:31:46 +0530

    task_9

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,138 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\category;
+use Illuminate\Support\Facades\Log;
 
 class CategoryController extends Controller
 {
-    //
+    public function post()
+    {
+        try {
+            $insert = Category::create([
+                "name" => "new",
+                "is_active" => 1
+            ]);
+            return response()->json([
+                "message" => "Success",
+                "result" => $insert
+            ], 200);
+        } catch (Exception $c) {
+            Log::error($c);
+        }
+    }
+
+    public function bulkInsert()
+
+    {
+
+        try {
+            $category = [
+
+                ['name' => 'Category 1', 'is_active' => true],
+
+                ['name' => 'Category 2', 'is_active' => true],
+
+                ['name' => 'Category 3', 'is_active' => true],
+
+                ['name' => 'Category 4', 'is_active' => true],
+
+                ['name' => 'Category 5', 'is_active' => true],
+
+                ['name' => 'Category 6', 'is_active' => true],
+
+                ['name' => 'Category 7', 'is_active' => true],
+
+                ['name' => 'Category 8', 'is_active' => true],
+
+                ['name' => 'Category 9', 'is_active' => true],
+
+                ['name' => 'Category 10', 'is_active' => true],
+
+            ];
+
+
+
+
+            Category::insert($category);
+
+
+
+
+            if ($category) {
+
+                Log::info("user", [$category]);
+
+                return response()->json([
+
+                    'message' => 'bulkInsert successfully',
+
+                    'result' => $category,
+
+                ], 200);
+            } else {
+
+                return response()->json([
+
+                    'message' => 'bulkInsert error',
+
+                ], 404);
+            }
+        } catch (Exception $e) {
+
+            Log::error('error', [$e]);
+
+            return response()->json([
+
+                "message" => "Internal server error",
+
+            ], 500);
+        }
+    }
+    public function get()
+    {
+        try {
+            $categories = Category::get();
+            return response()->json([
+                "message" => "Success",
+                "result" => $categories
+            ], 200);
+        } catch (Exception $c) {
+            Log::error($c);
+        }
+    }
+
+    public function put()
+    {
+        try {
+            $update = Category::wherein("id", [48,49])->update([
+                 'is_active' => false
+            ]);
+            return response()->json([
+                "message" => "Success",
+                "result" => $update
+            ], 200);
+        } catch (Exception $c) {
+            Log::error($c);
+        }
+    }
+
+    public function delete()
+    {
+        try {
+            $delete = Category::where('is_active', false)->delete([
+                "is_active" => 0
+            ]);
+            return response()->json([
+                "message" => "Success",
+                "result" => $delete
+            ], 200);
+        } catch (Exception $c) {
+            Log::error($c);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\formcontroller;
@@ -31,10 +32,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::post("/post",returnValue("welcome"));
 
-Route::post("/form", [formcontroller::class,"insert"]);
-Route::get("/forms/{email}", [formcontroller::class,"user_lists"]);
-Route::put("/edit", [formcontroller::class,"update"]);
-Route::delete("/del", [formcontroller::class,"destroy"]);
+// Route::post("/form", [formcontroller::class,"insert"]); //task_8
+// Route::get("/forms/{email}", [formcontroller::class,"user_lists"]);
+// Route::put("/edit", [formcontroller::class,"update"]);
+// Route::delete("/del", [formcontroller::class,"destroy"]);
+
+
+Route::post("/category", [CategoryController::class,"post"]);
+Route::post("/categories", [CategoryController::class,"bulkInsert"]);
+Route::get("/categories", [CategoryController::class,"get"]);
+Route::put("/category", [CategoryController::class,"put"]);
+Route::delete("/category", [CategoryController::class,"delete"]);
 
 
   
